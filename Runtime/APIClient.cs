91ba2d77e21dbe01e40a396b7697168743b9116f
Victor Bisterfeld <gaabviktor@gmail.com>
2024-10-14 12:17:54 +0200

    feat: add retry request on error by SendAsync method

--- Runtime/APIClient.cs
+++ Runtime/APIClient.cs
@@ -219,13 +219,43 @@ namespace MontanaGames.JsonAPIClient
                 {
                     case HttpStatusCode.OK:
                     {
-                        IsConnected = true;
-                        var responseObject = serializer.Deserialize<ResponseType>(reader);
-                    
-                        var headers = response.Headers;
-                        ApplyBearerTokenFromResponseHeader(headers);
-                    
-                        return responseObject;
+                        try
+                        {
+                            IsConnected = true;
+                            var responseObject = serializer.Deserialize<ResponseType>(reader);
+
+                            var headers = response.Headers;
+                            ApplyBearerTokenFromResponseHeader(headers);
+
+                            return responseObject;
+                        } catch (Exception ex)
+                        {
+                            Debug.LogError(requestUrl);
+                            Debug.LogError(reader.ReadAsString());
+
+                            await UniTask.Delay(TimeSpan.FromMilliseconds(100));
+                            
+							response = await client.PostAsync(requestUrl,
+                    			new StringContent(ToJsonString(data), Encoding.UTF8));
+
+                			responseStream = await response.Content.ReadAsStreamAsync();
+
+                			using var sr2 = new StreamReader(responseStream);
+                			using JsonReader reader2 = new JsonTextReader(sr);
+
+                			statusCode = response.StatusCode;
+
+             				switch (statusCode)
+                			{
+                    			case HttpStatusCode.OK:
+                    			{
+                            		IsConnected = true;
+                            		var responseObject2 = serializer.Deserialize<ResponseType>(reader2);
+									return responseObject2;
+								}
+							}
+                            throw;
+                        }
                     }
                     case HttpStatusCode.Unauthorized:
                         onUnauthorized?.Invoke();
